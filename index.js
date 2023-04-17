63f059ae4962cc6320835d9312502e82e6afda7f
Kennedy Musau <kennedy.musauu@gmail.com>
2023-04-17 21:38:51 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+function reversedString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reversedString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+
+  reverse the input string
+
+  if the reversed string is the name as the input
+    return true
+  eles
+    return false
 */
 
 /*
@@ -15,6 +29,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
